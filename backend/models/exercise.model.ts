6f5de3eb80504ff4dbc074f14f50cdc266bec0e4
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-18 08:54:17 +0000

    add exercise schema

---
+++ backend/models/exercise.model.ts
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+
+const exerciseSchema = new Schema({
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+  }, {
+    timestamps: true,
+})
