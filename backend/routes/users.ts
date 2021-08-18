85e0098d7c96c8b44f90880ca7e884135ab424d2
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-18 09:30:25 +0000

    add routes and use ES6 syntax

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from 'express';
+import { User } from '../models/user.model'
 const router = require('express').Router();
-const User = require('../models/user.model');
 
 router.route('/').get((req: Request, res: Response) => {
     User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
+        .then((users) => res.json(users))
+        .catch((err: Error) => res.status(400).json('Error: ' + err));
 });
+
+router.route('/add').post((req: Request, res: Response) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+    .then(() => res.json('User added!'))
+    .catch((err: Error) => res.status(400).json('Error: ' + err));
+});
+
+export { router as usersRouter }
