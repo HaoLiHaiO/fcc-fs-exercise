1b9feae2a57bbea452e859f627acc4716d8ad130
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-18 10:17:53 +0000

    add post req for exerciseRouter

--- backend/routes/exercises.ts
+++ backend/routes/exercises.ts
@@ -8,4 +8,22 @@ router.route('/').get((req: Request, res: Response) => {
     .catch((err: Error) => res.status(400).json('Error: '+ err));
 });
 
+router.route('/add').post((req: Request, res: Response) => {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = req.body.duration;
+    const date = Date.parse(req.body.date);
+
+    const newExercise = new Exercise({
+        username,
+        description,
+        duration,
+        date
+    });
+
+    newExercise.save()
+    .then(() => res.json('Exercise added!'))
+    .catch((err: Error) => res.status(400).json('Error: ' + err));
+})
+
 export { router as exerciseRouter }
